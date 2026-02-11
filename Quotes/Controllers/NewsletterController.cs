e0935b2974bb4e116b0c6738b6ed7e3770ca9ed7
Karin Roman <karin.roman@gmail.com>
2026-02-11 19:41:39 +0100

    Subscribe to Newsletter

---
+++ Quotes/Controllers/NewsletterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quotes.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        public IActionResult Subscribe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Subscribe(string name, string email)
+        {
+            return Content($"THANX {name}! You will be hearing from us :)");
+        }
+    }
+}
