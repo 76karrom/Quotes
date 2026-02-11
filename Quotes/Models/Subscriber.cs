cec2b58e2785b20af917ed5fc364ff66be3a42c6
Karin Roman <karin.roman@gmail.com>
2026-02-11 19:56:41 +0100

    validation

--- Quotes/Models/Subscriber.cs
+++ Quotes/Models/Subscriber.cs
@@ -1,9 +1,15 @@
-﻿namespace Quotes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quotes.Models
 {
     public class Subscriber
     {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name cannot exceed 20 characters")]
         public string? Name { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
     }
 }
